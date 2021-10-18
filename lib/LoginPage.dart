eb2267c47fa9f49915d79423ed76203a9a9ffe4d
kahjee <kagieangelomaruyama@gmail.com>
2021-10-18 13:53:28 +0800

    Login screen foundation, not yet done, and splash screen.

---
+++ lib/LoginPage.dart
@@ -0,0 +1,18 @@
+// ignore: file_names
+// ignore_for_file: file_names, prefer_const_literals_to_create_immutables
+
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      body:  Container(
+        width: double.infinity, 
+        decoration: BoxDecoration(
+        color: Colors.blue,
+        ))
+    );
+  }
+
+}
