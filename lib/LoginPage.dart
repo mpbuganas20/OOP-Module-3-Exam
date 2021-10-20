230e249694d2ce99097b9882d81bfa5f265c707e
kahjee <kagieangelomaruyama@gmail.com>
2021-10-20 15:14:19 +0800

    Login screen (message me if needs changes)

--- lib/LoginPage.dart
+++ lib/LoginPage.dart
@@ -1,18 +1,86 @@
-// ignore: file_names
-// ignore_for_file: file_names, prefer_const_literals_to_create_immutables
+// ignore_for_file: avoid_print, file_names
 
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget{
+class LoginPage extends StatelessWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
-      body:  Container(
-        width: double.infinity, 
-        decoration: BoxDecoration(
-        color: Colors.blue,
-        ))
+      body: Padding(
+        padding: const EdgeInsets.all(30),
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Image.asset('assets/images/logo.png'),
+
+                SizedBox(
+                  height: 30,
+                ),
+
+                TextFormField(
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                    labelText: 'Username',
+                    border: OutlineInputBorder(),
+                    prefixIcon: Icon(Icons.person)
+                  ),
+                ),
+
+                SizedBox(
+                  height: 30,
+                ),
+
+                TextFormField(
+                  keyboardType: TextInputType.visiblePassword,
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                    labelText: 'Password',
+                    border: OutlineInputBorder(),
+                    prefixIcon: Icon(Icons.lock),
+                    suffixIcon: Icon(Icons.remove_red_eye),
+                  ),
+                ),
+
+                SizedBox(
+                  height: 15,
+                ),
+
+                Container(
+                  clipBehavior: Clip.antiAliasWithSaveLayer,
+                  width: double.infinity,
+                  height: 60,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(100),
+                  ),
+
+                  child: MaterialButton(
+                    onPressed: () => print("Successul Login."),
+                    color: Colors.blue,
+                    child: Text(
+                      'Log In',
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+
+                SizedBox(
+                  height: 30,
+                ),
+                
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                    ),
+                  ],
+                ),
+            ),
+          ),
+        ),
     );
   }
-
 }
