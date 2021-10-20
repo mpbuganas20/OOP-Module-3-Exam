c269ee15435c35a4b60e5946887708eb8843db9f
kahjee <kagieangelomaruyama@gmail.com>
2021-10-20 20:21:43 +0800

    Changed login screen, added Authentication.dart, SplashScreen.dart, Dashboard.dart.

--- lib/LoginPage.dart
+++ lib/LoginPage.dart
@@ -1,38 +1,72 @@
-// ignore_for_file: avoid_print, file_names
-
 import 'package:flutter/material.dart';
+import 'Dashboard.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
 
   @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginPage> {
+  @override
   Widget build(BuildContext context) {
+    final TextEditingController userController = TextEditingController();
+    final TextEditingController passwordController = TextEditingController();
+
+    void login(String username, String password) {
+      if (username == "admin" && password == "admin") {
+        Navigator.push(context,
+            MaterialPageRoute(builder: (context) => const Dashboard()));
+      } else {
+        final snackBar = const SnackBar(
+            backgroundColor: Color(0xFFD50000),
+            content: Text("Error: Please use admin credentials for login.",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    color: Colors.white, fontWeight: FontWeight.w900)));
+
+        ScaffoldMessenger.of(context).showSnackBar(snackBar);
+      }
+    }
+
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(30),
-        child: Center(
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                Image.asset('assets/images/logo.png'),
-
-                SizedBox(
-                  height: 30,
-                ),
-
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text('Login Screen.'),
+          automaticallyImplyLeading: false,
+        ),
+        body: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 15),
+            child:
+                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+              Expanded(
+                  child: ListView(
+                children: [
+                  Container(
+                    height: 100,
+                    width: 100,
+                    decoration: const BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage('assets/images/logo.png'),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.only(top: 40),
+                    padding: const EdgeInsets.only(bottom: 25),
+                  ),
+                  
                 TextFormField(
-                  keyboardType: TextInputType.emailAddress,
                   decoration: InputDecoration(
                     labelText: 'Username',
                     border: OutlineInputBorder(),
                     prefixIcon: Icon(Icons.person)
                   ),
                 ),
-
-                SizedBox(
-                  height: 30,
-                ),
-
+                  
+                  const Padding(padding: EdgeInsets.all(15)),
+                  
                 TextFormField(
                   keyboardType: TextInputType.visiblePassword,
                   obscureText: true,
@@ -43,44 +77,36 @@ class LoginPage extends StatelessWidget {
                     suffixIcon: Icon(Icons.remove_red_eye),
                   ),
                 ),
-
-                SizedBox(
-                  height: 15,
-                ),
-
-                Container(
-                  clipBehavior: Clip.antiAliasWithSaveLayer,
-                  width: double.infinity,
-                  height: 60,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(100),
-                  ),
-
-                  child: MaterialButton(
-                    onPressed: () => print("Successul Login."),
-                    color: Colors.blue,
-                    child: Text(
-                      'Log In',
-                      style: TextStyle(
-                        fontSize: 20,
-                        color: Colors.white,
+                  
+                  Align(
+                    alignment: Alignment.topCenter,
+                    child: Container(
+                      height: 75,
+                      width: 100,
+                      margin: const EdgeInsets.all(25),
+                      child: ElevatedButton(
+                        child:
+                            const Text('Login', style: TextStyle(fontSize: 16)),
+                        onPressed: () {
+                          login(userController.text, passwordController.text);
+                        },
+                        style: ButtonStyle(
+                          shape: MaterialStateProperty.all(
+                            RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                          ),
+                          padding: MaterialStateProperty.all(
+                              const EdgeInsets.all(15)),
+                          elevation: MaterialStateProperty.all(0),
+                          backgroundColor: MaterialStateProperty.all(
+                              const Color(0xFF2196F3)),
+                        ),
                       ),
                     ),
-                  ),
-                ),
-
-                SizedBox(
-                  height: 30,
-                ),
-                
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                    ),
-                  ],
-                ),
-            ),
-          ),
-        ),
-    );
+                  )
+                ],
+              )),
+            ])));
   }
 }
